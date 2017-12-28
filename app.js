5d8d3950f34e0a136377d57017cd65a032ecf436
mehrimo <mehrimova@gmail.com>
2017-12-28 13:52:18 -0700

    clean up code

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 "use strict";
-
 (function(){
+
 var tableNumber = $("#table-number");
 var cartItems = [];
 var orderTotals = {
@@ -13,104 +13,102 @@ var orderTotals = {
 buildTables();
 buildMenu();
 
-function buildTables() {
-        $.ajax({
-            url: 'https://check-api.herokuapp.com/tables',
-            contentType: 'application/json',
-            type: 'GET',
-            headers: {
-              "Content-Type":"application/json",
-              "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImVlNTc2MzhmLWI0YWItNDhmZS1hNjdlLTYwMzNiNjkwMWRiZiIsIm5hbWUiOiJNZWhyaSJ9.qMKQh_xECSDEsRrZMzkWmbr3WKtfbSKF7a8xDxBvxYM"
-            },
-            success: function(response) {
-              console.log(JSON.parse(response));
-                var finalResponse = JSON.parse(response);
+  function buildTables() {
+      $.ajax({
+        url: 'https://check-api.herokuapp.com/tables',
+        contentType: 'application/json',
+        type: 'GET',
+        headers: {
+          "Content-Type":"application/json",
+          "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImVlNTc2MzhmLWI0YWItNDhmZS1hNjdlLTYwMzNiNjkwMWRiZiIsIm5hbWUiOiJNZWhyaSJ9.qMKQh_xECSDEsRrZMzkWmbr3WKtfbSKF7a8xDxBvxYM"
+        },
+        success: function(response) {
+          console.log(JSON.parse(response));
+            var finalResponse = JSON.parse(response);
 
-                finalResponse.forEach(function(table) {
-                  $('#table-number').append('<option>' +
-                  table.number + '</option>');
-                });
-
-            }
-        });
-
-        $('#table-btn').on('click', function(event) {
-          var table = tableNumber.val();
-          event.preventDefault();
-          console.log("The table number is: " + table);
-        });
-
-    }
-
-    function buildMenu() {
-            $.ajax({
-                url: 'https://check-api.herokuapp.com/items',
-                contentType: 'application/json',
-                type: 'GET',
-                headers: {
-                  "Content-Type":"application/json",
-                  "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImVlNTc2MzhmLWI0YWItNDhmZS1hNjdlLTYwMzNiNjkwMWRiZiIsIm5hbWUiOiJNZWhyaSJ9.qMKQh_xECSDEsRrZMzkWmbr3WKtfbSKF7a8xDxBvxYM"
-                },
-                success: function(response) {
-                  console.log(JSON.parse(response));
-                    var finalItems = JSON.parse(response);
-
-                    finalItems.forEach(function(item) {
-                        $('#menu-items').append('<li>' + item.name + '- $' + item.price + '<button id="' +
-                        item.id +
-                        '" class="add-item btn">Add</button></li>');
-
-                      $('.add-item').on('click', function(event) {
-                        event.preventDefault();
-                        addToCart(item);
-                        console.log(item, id);
-                        });
-                    });
-                }
+            finalResponse.forEach(function(table) {
+              $('#table-number').append('<option>' +
+              table.number + '</option>');
             });
+
         }
+    });
 
-
-    function addToCart(item){
-      cartItems.push({
-        item: item,
-        id: item.id,
-        quantity: 1,
+      $('#table-btn').on('click', function(event) {
+        var table = tableNumber.val();
+        event.preventDefault();
+        console.log("The table number is: " + table);
       });
-      drawCart(cartItems);
-    }
+  }
 
-    function drawCart(cartItems){
-      var cart = $(".cartItems");
-      cart.html('');
-      cartItems.forEach(function(cartItem){
-        cart.append(
-          '<tr>'+
-            '<td>' + cartItem.item.name + '</td>'+
-            '<td class="right-align">' + formatCurrency(cartItem.item.price) + '</td>' +
-          '</tr>'
-        );
+  function buildMenu() {
+      $.ajax({
+          url: 'https://check-api.herokuapp.com/items',
+          contentType: 'application/json',
+          type: 'GET',
+          headers: {
+            "Content-Type":"application/json",
+            "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImVlNTc2MzhmLWI0YWItNDhmZS1hNjdlLTYwMzNiNjkwMWRiZiIsIm5hbWUiOiJNZWhyaSJ9.qMKQh_xECSDEsRrZMzkWmbr3WKtfbSKF7a8xDxBvxYM"
+          },
+          success: function(response) {
+            console.log(JSON.parse(response));
+              var finalItems = JSON.parse(response);
+
+              finalItems.forEach(function(item) {
+                  $('#menu-items').append('<li>' + item.name + '- $' + item.price + '<button id="' +
+                  item.id +
+                  '" class="add-item btn">Add</button></li>');
+
+                $('.add-item').on('click', function(event) {
+                  event.preventDefault();
+                  addToCart(item);
+                  console.log(item, id);
+                  });
+              });
+          }
       });
-      calculate();
-    }
+  }
 
-    function formatCurrency(number) {
-      return '$' + number.toFixed(2);
-    }
+  function addToCart(item){
+    cartItems.push({
+      item: item,
+      id: item.id,
+      quantity: 1,
+    });
+    drawCart(cartItems);
+  }
 
-    function calculate() {
-      var subtotal = cartItems.reduce(function(accumulator, cartItems){
-        return accumulator + cartItems.item.price * cartItems.quantity;
-      }, 0);
-      var tax = subtotal * 0.08845;
-      var total = subtotal + tax;
-      var tip = total * 0.20;
-      orderTotals.subtotal = subtotal;
-      orderTotals.tax = tax;
-      orderTotals.total = total;
-      orderTotals.tip = tip;
-      drawOrderTotal();
-    }
+  function drawCart(cartItems){
+    var cart = $(".cartItems");
+    cart.html('');
+    cartItems.forEach(function(cartItem){
+      cart.append(
+        '<tr>'+
+          '<td>' + cartItem.item.name + '</td>'+
+          '<td class="right-align">' + formatCurrency(cartItem.item.price) + '</td>' +
+        '</tr>'
+      );
+    });
+    calculate();
+  }
+
+  function formatCurrency(number) {
+    return '$' + number.toFixed(2);
+  }
+
+  function calculate() {
+    var subtotal = cartItems.reduce(function(accumulator, cartItems){
+      return accumulator + cartItems.item.price * cartItems.quantity;
+    }, 0);
+    var tax = subtotal * 0.08845;
+    var total = subtotal + tax;
+    var tip = total * 0.20;
+    orderTotals.subtotal = subtotal;
+    orderTotals.tax = tax;
+    orderTotals.total = total;
+    orderTotals.tip = tip;
+    drawOrderTotal();
+  }
 
   function drawOrderTotal() {
     var totals = $(".orderTotals");
--- server.js
+++ server.js
@@ -4,11 +4,6 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-var tables = [];
-var items = [];
-var checks = [];
-
-
 var PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
